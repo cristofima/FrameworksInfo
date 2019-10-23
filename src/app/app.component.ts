981b36fb67c1976228be1a7aa67251ea5f17fa59
Cristopher Coronado <cristofima@hotmail.com>
2019-10-23 10:34:52 -0500

    Init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {GithubService} from './shared/github.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'FrameworksInfo';
+export class AppComponent implements OnInit{
+  private repositories: any[] = [
+    {name: 'Java Server Faces', repo:'javaserverfaces/mojarra', language: 'Java'},
+    {name: 'Angular', repo:'angular/angular', language: 'JavaScript'},
+    {name: 'Ionic', repo:'ionic-team/ionic', language: 'Java'},
+    {name: 'Xamarin Forms', repo:'xamarin/Xamarin.Forms', language: 'C#'},
+    {name: 'Node.js', repo:'nodejs/node', language: 'JavaScript'},
+    {name: '.NET Core', repo:'dotnet/core', language: 'C#'},
+    {name: 'React', repo:'facebook/reacts', language: 'JavaScript'},
+    {name: 'Ruby on Rails', repo:'rails/rails', language: 'Ruby'},
+    {name: 'Django', repo:'django/django', language: 'Python'},
+    {name: 'Next.js', repo:'zeit/next.js', language: 'JavaScript'},
+    {name: 'Spring Boot', repo:'spring-projects/spring-boot', language: 'Java'},
+    {name: 'Spring Framework', repo:'spring-projects/spring-framework', language: 'Java'},
+    {name: 'Mojolicious', repo:'mojolicious/mojo', language: 'Perl'},
+    {name: 'Flutter', repo:'flutter/flutter', language: 'JavaScript'},
+    {name: 'Aurelia', repo:'aurelia/framework', language: 'JavaScript'},
+    {name: 'Yii', repo:'yiisoft/yii2', language: 'PHP'},
+    {name: 'Symfony', repo:'symfony/symfony', language: 'PHP'},
+  ];
+
+  frameworks: any[] = [];
+
+  constructor(private githubService: GithubService){
+
+  }
+
+  ngOnInit() {
+    this.frameworks = [];
+    for(const rep of this.repositories){
+      this.githubService.getLastestRelease(rep.repo).subscribe((data: any) => {
+        data.framework_name = rep.name;
+        data.language = rep.language;
+        this.frameworks.push(data);
+      }, error => {
+
+      });
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { HttpClientModule} from '@angular/common/http';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import {MatTableModule} from '@angular/material/table';
 
 @NgModule({
   declarations: [
@@ -10,7 +15,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatTableModule
   ],
   providers: [],
   bootstrap: [AppComponent]
