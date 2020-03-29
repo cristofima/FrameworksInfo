71d9fe00d408d5e6626bded61319815e2acee6f3
Cristopher Coronado Moreira <cristofima@hotmail.com>
2020-03-29 12:24:27 -0500

    Fix issue with CORS

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,48 +1,117 @@
 import { Component, OnInit } from '@angular/core';
-import {GithubService} from './shared/github.service';
+import { GithubService } from './shared/github.service';
+import { DependencyInfoService } from './shared/dependency-info.service';
+
+import * as firebase from 'firebase';
+import { environment } from '../environments/environment';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit{
+export class AppComponent implements OnInit {
   private repositories: any[] = [
-    {name: 'Java Server Faces', repo:'javaserverfaces/mojarra', language: 'Java'},
-    {name: 'Angular', repo:'angular/angular', language: 'JavaScript'},
-    {name: 'Ionic', repo:'ionic-team/ionic', language: 'Java'},
-    {name: 'Xamarin Forms', repo:'xamarin/Xamarin.Forms', language: 'C#'},
-    {name: 'Node.js', repo:'nodejs/node', language: 'JavaScript'},
-    {name: '.NET Core', repo:'dotnet/core', language: 'C#'},
-    {name: 'React', repo:'facebook/reacts', language: 'JavaScript'},
-    {name: 'Ruby on Rails', repo:'rails/rails', language: 'Ruby'},
-    {name: 'Django', repo:'django/django', language: 'Python'},
-    {name: 'Next.js', repo:'zeit/next.js', language: 'JavaScript'},
-    {name: 'Spring Boot', repo:'spring-projects/spring-boot', language: 'Java'},
-    {name: 'Spring Framework', repo:'spring-projects/spring-framework', language: 'Java'},
-    {name: 'Mojolicious', repo:'mojolicious/mojo', language: 'Perl'},
-    {name: 'Flutter', repo:'flutter/flutter', language: 'JavaScript'},
-    {name: 'Aurelia', repo:'aurelia/framework', language: 'JavaScript'},
-    {name: 'Yii', repo:'yiisoft/yii2', language: 'PHP'},
-    {name: 'Symfony', repo:'symfony/symfony', language: 'PHP'},
+    { name: 'Java Server Faces', repo: 'javaserverfaces/mojarra', language: 'Java' },
+    { name: 'Angular', repo: 'angular/angular', language: 'JavaScript', nameDependency: '@angular/core' },
+    { name: 'Ionic', repo: 'ionic-team/ionic', language: 'Java', nameDependency: 'Ionic' },
+    { name: 'Xamarin Forms', repo: 'xamarin/Xamarin.Forms', language: 'C#', nameDependency: 'Xamarin.Forms' },
+    { name: 'Node.js', repo: 'nodejs/node', language: 'JavaScript', nameDependency: 'node' },
+    { name: '.NET Core', repo: 'dotnet/core', language: 'C#', nameDependency: 'Microsoft.NETCore.Targets' },
+    { name: 'React', repo: 'facebook/reacts', language: 'JavaScript' },
+    { name: 'Ruby on Rails', repo: 'rails/rails', language: 'Ruby' },
+    { name: 'Django', repo: 'django/django', language: 'Python', nameDependency: 'Django' },
+    { name: 'Flask', repo: 'pallets/flask', language: 'Python', nameDependency: 'Flask' },
+    { name: 'Pyramid', repo: 'Pylons/pyramid', language: 'Python', nameDependency: 'Pyramid' },
+    { name: 'Web2py ', repo: 'web2py/web2py', language: 'Python', nameDependency: 'Web2py' },
+    { name: 'Next.js', repo: 'zeit/next.js', language: 'JavaScript' },
+    { name: 'Spring Boot', repo: 'spring-projects/spring-boot', language: 'Java' },
+    { name: 'Spring Framework', repo: 'spring-projects/spring-framework', language: 'Java' },
+    { name: 'Mojolicious', repo: 'mojolicious/mojo', language: 'Perl' },
+    { name: 'Flutter', repo: 'flutter/flutter', language: 'JavaScript' },
+    { name: 'Aurelia', repo: 'aurelia/framework', language: 'JavaScript' },
+    { name: 'Yii 2', repo: 'yiisoft/yii2', language: 'PHP', nameDependency: 'yiisoft/yii2' },
+    { name: 'Symfony', repo: 'symfony/symfony', language: 'PHP', nameDependency: 'symfony/symfony' },
+    { name: 'Laravel', repo: 'laravel/laravel', language: 'PHP', nameDependency: 'laravel/framework' },
   ];
 
   frameworks: any[] = [];
 
-  constructor(private githubService: GithubService){
-
+  constructor(private dependencyInfoService: DependencyInfoService) {
+    firebase.initializeApp(environment.firebaseConfig);
   }
 
   ngOnInit() {
     this.frameworks = [];
-    for(const rep of this.repositories){
-      this.githubService.getLastestRelease(rep.repo).subscribe((data: any) => {
+    for (const rep of this.repositories) {
+      if (rep.nameDependency) {
+        if (rep.language === 'JavaScript') {
+          this.dependencyInfoService.getNPMInfo(rep.nameDependency).subscribe((data: any) => {
+            let dataFramework = {
+              framework_name: rep.name,
+              tag_name: data['dist-tags'].latest,
+              language: rep.language,
+              published_at: ''
+            }
+
+            this.frameworks.push(dataFramework);
+          });
+        } else if (rep.language === 'Python') {
+          this.dependencyInfoService.getPypiInfo(rep.nameDependency).subscribe((resp: any) => {
+            let dataFramework = {
+              framework_name: rep.name,
+              tag_name: resp.info.version,
+              language: rep.language,
+              published_at: resp.releases[resp.info.version][0].upload_time
+            }
+
+            this.frameworks.push(dataFramework);
+          });
+        } else if (rep.language === 'C#') {
+          this.dependencyInfoService.getNugetInfo(rep.nameDependency).subscribe((resp: any) => {
+            let dataFramework = {
+              framework_name: rep.name,
+              tag_name: resp.data[0].version,
+              language: rep.language,
+              published_at: ''
+            }
+
+            this.frameworks.push(dataFramework);
+          });
+        } else if (rep.language === 'PHP') {
+          this.dependencyInfoService.getPackagistInfo(rep.nameDependency).subscribe((resp: any) => {
+            let versionsInfo = resp.packages[rep.nameDependency];
+            let versionKeys = Object.keys(versionsInfo);
+            let lastVersionInfo = versionsInfo[versionKeys[versionKeys.length - 1]];
+
+            let dataFramework = {
+              framework_name: rep.name,
+              tag_name: lastVersionInfo.version_normalized,
+              language: rep.language,
+              published_at: lastVersionInfo.time
+            }
+
+            this.frameworks.push(dataFramework);
+          });
+        }
+      }
+      /*this.githubService.getLastestRelease(rep.repo).subscribe((data: any) => {
         data.framework_name = rep.name;
         data.language = rep.language;
-        this.frameworks.push(data);
+        //this.frameworks.push(data);
+
+        const fr = firebase.database().ref('/frameworks').push();
+        fr.set(data);
       }, error => {
 
-      });
+      });*/
     }
+    /*firebase.database().ref('/frameworks').on('value', resp => {
+      resp.forEach((childSnapshot: any) => {
+        const item = childSnapshot.val();
+        item.key = childSnapshot.key;
+        this.frameworks.push(item);
+      });
+    });*/
   }
 }
---
+++ src/app/shared/dependency-info.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DependencyInfoService {
+
+  constructor(private http: HttpClient) {
+
+  }
+
+  public getNPMInfo(dependency: string) {
+    return this.http.get(`https://cors-anywhere.herokuapp.com/https://registry.npmjs.org/${dependency}`);
+  }
+
+  public getPypiInfo(dependency: string) {
+    return this.http.get(`https://pypi.org/pypi/${dependency}/json`);
+  }
+
+  public getNugetInfo(dependency: string) {
+    return this.http.get(`https://cors-anywhere.herokuapp.com/https://azuresearch-usnc.nuget.org/query?q=${dependency}&prerelease=false`);
+  }
+
+  public getPackagistInfo(dependency: string) {
+    return this.http.get(`https://repo.packagist.org/p/${dependency}.json`);
+  }
+}
