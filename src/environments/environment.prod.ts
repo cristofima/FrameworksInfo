01a8d24605db58a16f4283a3863d1956c7d5bff8
Cristopher Coronado Moreira <cristofima@hotmail.com>
2020-03-29 12:28:05 -0500

    Firebase setup

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyAcUTULg-pIS4NCb4oe8TOF89oao3FgogQ",
+    authDomain: "frameworks-info.firebaseapp.com",
+    databaseURL: "https://frameworks-info.firebaseio.com",
+    projectId: "frameworks-info",
+    storageBucket: "frameworks-info.appspot.com",
+    messagingSenderId: "126809825110",
+    appId: "1:126809825110:web:f0857bc371e8f6670280f7",
+    measurementId: "G-58ZHFFW6RM"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyAcUTULg-pIS4NCb4oe8TOF89oao3FgogQ",
+    authDomain: "frameworks-info.firebaseapp.com",
+    databaseURL: "https://frameworks-info.firebaseio.com",
+    projectId: "frameworks-info",
+    storageBucket: "frameworks-info.appspot.com",
+    messagingSenderId: "126809825110",
+    appId: "1:126809825110:web:f0857bc371e8f6670280f7",
+    measurementId: "G-58ZHFFW6RM"
+  }
 };
 
 /*
